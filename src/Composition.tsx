696a85e36f73ddc61a88f4e29b8ee4057a8a41dd
aryanbisht <aryansbisht@outlook.com>
2022-08-29 09:28:08 -0400

    Composition.tsx, package.json and Video.tsx. Video.tsx has all the video transitions and effects.

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,28 @@
+import { useVideoConfig, useCurrentFrame } from "remotion";
+
+
 export const MyComposition = () => {
-	return null;
+	const {fps, durationInFrames, width, height} = useVideoConfig();
+
+	const frame = useCurrentFrame();
+	const opacity = frame / durationInFrames;
+	return (
+		<div
+			style={{
+				flex: 1,
+				textAlign: 'center',
+				fontSize: '7em',
+				background: 'cyan',
+				color: 'black',
+				opacity,
+			}}
+			>
+			Hello DevEffect
+			This video is using {width}x{height} px
+			Being {durationInFrames/fps} seconds long
+
+			{frame}
+
+		</div>
+	);
 };
